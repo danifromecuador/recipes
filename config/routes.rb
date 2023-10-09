2937d3ca7d607798b4c6f57b23cc715928d34876
gersongahg <gahg.10gerson@gmail.com>
2023-10-09 13:26:38 -0400

    Login and Register functionallity

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+  registrations: 'registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: "users#index"
 end
