80f0cd1ef22197bdd1bfdb9ef8b6d0994fd8ac84
dani <danifromecuador@gmail.com>
2023-10-10 13:41:06 -0500

    Style root and Improve UX

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,32 +11,36 @@ class User < ApplicationRecord
 
   def generate_shopping_list
     recipes = self.recipes
-    user_food_list = Food.where(user_id: self.id)
+    user_food_list = Food.where(user_id: id)
     missing_food_list = []
     total_food_items = 0
     total_price = 0
     recipes.each do |recipe|
       recipe.foods.each do |food|
-        if user_food_list.where(id: food.id).empty? || user_food_list.find(food.id).quantity < food.recipe_foods.find_by(recipe_id: recipe.id).quantity
-          missing_food_list << food
-          total_food_items += 1
-          total_price += food.price * (food.recipe_foods.find_by(recipe_id: recipe.id).quantity - user_food_list.find(food.id).quantity)
+        unless user_food_list.where(id: food.id).empty? ||
+               user_food_list.find(food.id).quantity < food.recipe_foods.find_by(recipe_id: recipe.id).quantity
+          next
         end
+
+        missing_food_list << food
+        total_food_items += 1
+        total_price += food.price * (food.recipe_foods.find_by(recipe_id: recipe.id).quantity
+                                     - user_food_list.find(food.id).quantity)
       end
     end
     shopping_list = []
     missing_food_list.each do |food|
       recipe_food = food.recipe_foods.find_by(recipe_id: recipes.pluck(:id))
-      if recipe_food
-        quantity_to_buy = recipe_food.quantity - user_food_list.find(food.id).quantity
-        price = quantity_to_buy * food.price
-        shopping_list << { food: food, quantity: quantity_to_buy, price: price }
-      end
+      next unless recipe_food
+
+      quantity_to_buy = recipe_food.quantity - user_food_list.find(food.id).quantity
+      price = quantity_to_buy * food.price
+      shopping_list << { food:, quantity: quantity_to_buy, price: }
     end
     {
-      shopping_list: shopping_list,
-      total_food_items: total_food_items,
-      total_price: total_price
+      shopping_list:,
+      total_food_items:,
+      total_price:
     }
   end
 end
