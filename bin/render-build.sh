ba76831926ca443957e0e324fe3917f0c6205800
dani <danifromecuador@gmail.com>
2024-06-26 17:09:44 -0500

    Drop, create, and migrate rails db | feat/deploy | bin/render-build.sh |

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,12 @@
 set -o errexit
 
 bundle install
+
+bundle exec rails db: drop
+bundle exec rails db: create
+bundle exec rails db: migrate
+
+
+
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
