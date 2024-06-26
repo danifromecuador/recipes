d98daf9aa7f975d39004c8d8417128996dec3e61
dani <danifromecuador@gmail.com>
2024-06-26 17:18:20 -0500

    Undo last commit | feat/deploy | bin/render-build.sh |

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,12 +3,5 @@
 set -o errexit
 
 bundle install
-
-bundle exec rails db: drop
-bundle exec rails db: create
-bundle exec rails db: migrate
-
-
-
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
