90181e562c7bf25503a27e6569223498e5a585f6
dani <danifromecuador@gmail.com>
2024-06-26 17:41:38 -0500

    Modification of script | feat/deploy | bin/render-build.sh |

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
+# Install dependencies
 bundle install
+
+# Prepare the database (create and migrate)
+bundle exec rails db:prepare
+
+# Precompile assets
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
