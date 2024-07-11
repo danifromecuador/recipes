c27ca2e553c615ea2c28cab39d689d92328ec628
dani <danifromecuador@gmail.com>
2024-07-11 18:51:49 -0500

    Modify render script to drop, create, and migrate the DB | feat/deploy | bin/render-build.sh |

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-# Exit on error
+# exit on error
 set -o errexit
 
 # Install dependencies
 bundle install
 
-# Prepare the database (create and migrate)
-bundle exec rails db:prepare
-
 # Precompile assets
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+bundle exec rake assets:precompile
+
+# Clean up assets
+bundle exec rake assets:clean
+
+# Drop, create, and migrate the database
+bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bundle exec rails db:create
+bundle exec rails db:schema:load
+bundle exec rails db:migrate
