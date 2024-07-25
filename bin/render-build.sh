fe2a528e6237ad587ca37a3f2d9e953ee7ccfff9
dani <danifromecuador@gmail.com>
2024-07-24 22:51:34 -0500

    Attempt 2 to share the same DB for two apps | feat/deploy | bin/render-build.sh |

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -12,7 +12,10 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 
 # Drop, create, and migrate the database
-bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-bundle exec rails db:create
-bundle exec rails db:schema:load
+# bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+# bundle exec rails db:create
+# bundle exec rails db:schema:load
+# bundle exec rails db:migrate
+
+# Only run migrations without dropping the database
 bundle exec rails db:migrate
